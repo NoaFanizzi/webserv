8bfd371a1e7d77e240feaac65c285b9697d2d106
NoaFanizzi <noafanizzistdy@gmail.com>
2026-01-29 16:58:56 +0100

    feat : changed inet_addr

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -3,6 +3,12 @@
 #include "ManageAll.hpp"
 #include "fcntl.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>      // Obligatoire pour getaddrinfo
+#include <cstring>      // Pour memset
+#include <stdexcept>    // Pour std::runtime_error (optionnel, pour gérer l'erreur)
+
 Server::Server(const ServerConfig &serverconfig) : _serverconfig(serverconfig)
 {
 	_closedStatus = false;
@@ -35,9 +41,23 @@ int	Server::createSocket()
 
 void Server::createSocketAdress(const ServerConfig &serverconfig)
 {
-	_servaddr.sin_family = AF_INET;
-	_servaddr.sin_addr.s_addr = inet_addr(serverconfig.host.c_str());
-	_servaddr.sin_port = htons(serverconfig.port);
+    struct addrinfo hints, *res;
+    int status;
+
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(serverconfig.host.c_str(), NULL, &hints, &res)) != 0) {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        throw std::runtime_error("Failed to resolve host IP");
+    }
+
+    _servaddr.sin_family = AF_INET;
+    _servaddr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+    _servaddr.sin_port = htons(serverconfig.port);
+
+    freeaddrinfo(res);
 }
 
 void Server::bindClient(int fd)
